4812fc15a0eae520e3c22314926b79010ebd1a5f
Barthold Bos <bartholdbos@hotmail.com>
2017-05-17 18:47:13 +0200

    download link

--- studiemeter_editor.user.js
+++ studiemeter_editor.user.js
@@ -2,8 +2,8 @@
 // @name          Studiemeter editor
 // @namespace     https://github.com/bartholdbos/studiemeter
 // @description   nodig om studiemeter script te bewerken
-// @version       0.1
+// @version       0.2
 // @match         https://www.studiemeter.nl/*
 // @require       file:///home/bbos/studiemeter/studiemeter.user.js
-// @downloadURL   https://raw.githubusercontent.com/bartholdbos/studiemeter/master/studiemeter_editor.user.js
+// @downloadURL   https://github.com/bartholdbos/studiemeter/raw/master/studiemeter_editor.user.js
 // ==/UserScript==
